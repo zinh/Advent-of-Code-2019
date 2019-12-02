ccb91cd981cc39495a26cdf95a2e8bb5ea41b3cd
Vinh Nguyen <vinhnx@zigexn.co.jp>
2019-12-02 16:03:14 +0900

    Day 2: problem 1

--- day02/prob1.c
+++ day02/prob1.c
@@ -27,15 +27,40 @@ int parse_input(char* line, int** out) {
   return i;
 }
 
+int run(int* opcodes, int size) {
+  int pointer = 0;
+  while (opcodes[pointer] != 99) {
+    int op = opcodes[pointer];
+    switch (op) {
+    case 1:
+    case 2:
+      {
+	int pos1 = opcodes[pointer + 1];
+	int pos2 = opcodes[pointer + 2];
+	int result_pos = opcodes[pointer + 3];
+	printf("pos1 = %d, pos2 = %d, result = %d\n", pos1, pos2, result_pos);
+	int r = op == 1 ? opcodes[pos1] + opcodes[pos2] : opcodes[pos1] * opcodes[pos2];
+	opcodes[result_pos] = r;
+	pointer += 4;
+	break;
+      }
+    default:
+      return 0;
+    }
+  }
+  return opcodes[0];
+}
+
 int
 main(){
   char* line;
   line = read_input("01.input");
   int* opcodes = NULL;
   int size =  parse_input(line, &opcodes);
-  for (int i = 0; i < size; i++)
-    printf("%d\n", opcodes[i]);
-  
+  opcodes[1] = 12;
+  opcodes[2] = 2;
+  int result = run(opcodes, size);
+  printf("Result = %d\n", result);
   free(line);
   free(opcodes);
   return 0;
