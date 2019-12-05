255d6afaaebd2b2c4cc2ddedcd63b043804a7fc6
Vinh Nguyen <vinhnx@zigexn.co.jp>
2019-12-05 12:08:50 +0900

    Day 4

---
+++ day04/prob2.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int
+digit_to_array(int n, int** out) {
+  int capp = 10;
+  int* result = malloc(sizeof(int) * capp);
+  int size = 0;
+  while (n > 0) {
+    result[size++] = n % 10;
+    n = n / 10;
+    if (size >= capp) {
+      capp += 10;
+      result = realloc(result, sizeof(int) * capp);
+    }
+  }
+  *out = result;
+  return size;
+}
+
+int
+is_non_decreasing(int* n, int size) {
+  for (int i = size- 1; i > 0; i-- ) {
+    if (n[i] > n[i - 1])
+      return 0;
+  }
+  return 1;
+}
+
+int
+two_adjacent(int* n, int size) {
+  int count = 0;
+  for (int i = size - 1; i > 0; i--) {
+    if (n[i] == n[i - 1]) {
+      if (i >= 2 && n[i - 1] == n[i - 2])
+	continue;
+      else if (i < size - 1 && n[i] == n[i + 1])
+	continue;
+      else
+	count++;
+    }
+  }
+  return count > 0;
+}
+
+int
+main(int argc, char** argv) {
+  if (argc != 3)
+    return 1;
+  int from = atoi(argv[1]);
+  int to = atoi(argv[2]);
+  int count = 0;
+  for(int i = from; i <= to; i++) {
+    int* digits = NULL;
+    int size = digit_to_array(i, &digits);
+    if (two_adjacent(digits, size) && is_non_decreasing(digits, size))
+      count++;
+    free(digits);
+  }
+  printf("Result: %d\n", count);
+  return 0;
+}
