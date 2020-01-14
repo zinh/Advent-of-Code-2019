076520f0dba413e624958fd398ab031a340a66e0
Vinh Nguyen <xuanvinhct@gmail.com>
2020-01-14 15:46:48 +0900

    Fix memory bug

--- day16/lib.c
+++ day16/lib.c
@@ -10,7 +10,7 @@ read_file(char *file_name, int **result) {
     return 0;
   char c;
   int size = 0, max_size = 10;
-  int *numbers = malloc(sizeof(int) * max_size);
+  int *numbers = (int *)malloc(sizeof(int) * max_size);
   while ((c = getc(fd)) != '\n') {
     if (size >= max_size) {
       max_size += 10;
@@ -25,7 +25,7 @@ read_file(char *file_name, int **result) {
 // [1,2,3] -> [1,2,3,1,2,3]
 int*
 repeat_pattern(int pattern[], int pattern_size, int size) {
-  int *result = malloc(sizeof(int) * size);
+  int *result = (int *)malloc(sizeof(int) * size);
   for(int i = 0; i < size; i++) {
     result[i] = pattern[i % pattern_size];
   }
@@ -36,7 +36,7 @@ repeat_pattern(int pattern[], int pattern_size, int size) {
 // pattern: [1,2,3], repeat_count: 3 -> [1,1,1,2,2,2,3,3,3]
 int*
 duplicate_pattern(int *pattern, int pattern_size, int repeat_count) {
-  int *result = malloc(sizeof(int) * pattern_size * repeat_count);
+  int *result = (int *)malloc(sizeof(int) * pattern_size * repeat_count);
   for(int i = 0; i < pattern_size; i++) {
     for (int j = 0; j < repeat_count; j++)
       result[repeat_count * i + j] = pattern[i];
@@ -46,7 +46,8 @@ duplicate_pattern(int *pattern, int pattern_size, int repeat_count) {
 
 int *extend_pattern(int *pattern, int pattern_size, int repeat_count, int input_size) {
   int *duplicated_pattern = duplicate_pattern(pattern, pattern_size, repeat_count);
-  int *repeated = repeat_pattern(duplicated_pattern, pattern_size * repeat_count, input_size + 1);
+  int *repeated = repeat_pattern(duplicated_pattern, pattern_size * repeat_count, input_size);
+  free(duplicated_pattern);
   return repeated;
 }
 
@@ -68,7 +69,7 @@ void print_phase(int *numbers, int size) {
 }
 
 int *calculate_phase(int *numbers, int size, int *pattern, int pattern_size) {
-  int *new_numbers = malloc(sizeof(int) * size);
+  int *new_numbers = (int *)malloc(sizeof(int) * size);
   for (int pos = 0; pos < size; pos++)
     new_numbers[pos] = calculate_element(numbers, size, pattern, pattern_size, pos + 1);
   return new_numbers;
